12066ce85babfcf61d27688a0c0cad2d34d693f5
HumairaMahi-03 <humairamumtahinmahi@gmail.com>
2024-05-08 12:00:16 +0600

    game2 updated

--- game2/main.cpp
+++ game2/main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 254, 0, 0, 255);
         SDL_RenderClear(renderer);
         SDL_RenderPresent(renderer);
     }
